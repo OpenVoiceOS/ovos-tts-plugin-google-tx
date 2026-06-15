5cf19a93666f261e39342ab1c795583e3c53cc50
JarbasAi <jarbasai@mailfence.com>
2026-06-15 23:30:40 +0100

    test: add TTS intelligibility e2e (ovoscope)

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
     license='Apache-2.0',
     packages=['ovos_tts_plugin_google_tx'],
     install_requires=required("requirements.txt"),
+    extras_require={'test': ['ovoscope[tts]', 'pytest']},
     zip_safe=True,
     include_package_data=True,
     classifiers=[
